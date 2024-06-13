25a6e9f94ba41546625fdd73fe163fa525da9ca6
IcatianTrout <laurenceboubou@hotmail.com>
2024-06-13 14:20:19 -0400

    tried to fix nstart bug

--- R/kmeans.nstart.R
+++ R/kmeans.nstart.R
@@ -1,7 +1,7 @@
 ## The clusGap function takes in its 'FUNcluster' argument a function that will perform
 ## the clustering but it does not allow us to set the arguments of that
 ## function, so we have to do so beforehand by defining a new function:
-kmeans.nstart <- function (x, k) {
+kmeans.nstart <- function (x, k, nstart = nstart, iter.max = iter.max) {
   return(kmeans(
     x = x,
     centers = k,
--- R/pam.nstart.R
+++ R/pam.nstart.R
@@ -2,7 +2,7 @@
 ## the clustering but it does not allow us to set the arguments of that
 ## function, so we have to do so beforehand by defining a new function:
 
-pam.nstart <- function (x, k) {
+pam.nstart <- function (x, k, metric = metric) {
   return(pam(
     x = x,
     k = k,
